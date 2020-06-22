d7ee0aeb3e785f59aa5a36aa39727d64b9d8863d
Mateusz-Kalwinski <mateucz27@gmail.com>
2020-06-22 21:37:04 +0200

    Form for adding a new user with vform vue. User table has few new columns

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -62,11 +62,48 @@
                         </button>
                     </div>
                     <div class="modal-body">
-                        ...
+                        <div class="form-group">
+                            <input v-model="form.name" type="text" name="name"
+                                   placeholder="Name"
+                                   class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                            <has-error :form="form" field="name"></has-error>
+                        </div>
+
+                        <div class="form-group">
+                            <input v-model="form.email" type="text" name="email"
+                                   placeholder="Email address"
+                                   class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                            <has-error :form="form" field="email"></has-error>
+                        </div>
+                        <div class="form-group">
+                            <textarea v-model="form.bio" type="text" name="bio"
+                                   placeholder="Short bio for user (optional)"
+                                      class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                            <has-error :form="form" field="bio"></has-error>
+                        </div>
+                        <div class="form-group">
+                            <select v-model="form.type" type="text" name="type"
+                                      placeholder="Short bio for user (optional)"
+                                      class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                                <option value="">Select user role</option>
+                                <option value="admin">Admin</option>
+                                <option value="user">User</option>
+                                <option value="author">Author</option>
+                            </select>
+                            <has-error :form="form" field="type"></has-error>
+                        </div>
+
+                        <div class="form-group">
+                            <input v-model="form.password" type="password" name="password"
+                                   placeholder="Password"
+                                   class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                            <has-error :form="form" field="password"></has-error>
+                        </div>
+
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-success">Create</button>
+                        <button type="button" class="btn btn-primary">Create</button>
                     </div>
                 </div>
             </div>
@@ -76,7 +113,20 @@
 </template>
 
 <script>
+    import Form from 'vform'
     export default {
+        data(){
+            return{
+                form: new Form({
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: '',
+                })
+            }
+        },
         mounted() {
             console.log('Component mounted.')
         }
